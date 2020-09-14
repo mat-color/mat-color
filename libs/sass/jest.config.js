8ea0762c67ecf3714c0ecb93d19bfb2f018de42f
IKatsuba <katsuba.igor@gmail.com>
2020-09-14 23:24:23 +0300

    chore(sass): change builder

--- libs/sass/jest.config.js
+++ libs/sass/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: 'scss',
+  name: 'sass',
   preset: '../../jest.config.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/scss',
+  coverageDirectory: '../../coverage/libs/sass',
 };
