1e8f9059141f88766a9239637de40556768426cc
IKatsuba <katsuba.igor@gmail.com>
2020-09-14 23:23:54 +0300

    chore(sass): change builder

--- libs/sass/jest.config.js
+++ libs/sass/jest.config.js
@@ -1,21 +1,14 @@
 module.exports = {
-  name: 'sass',
+  name: 'scss',
   preset: '../../jest.config.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer',
-      ],
     },
   },
-  coverageDirectory: '../../coverage/libs/sass',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/scss',
 };
